b2ad0b9112584141b89501c8e2c871be7d7e910d
fvd2 <fmvd1931@outlook.com>
2021-10-10 18:29:13 +0200

    server setting update

--- server/index.js
+++ server/index.js
@@ -1,16 +1,11 @@
 const path = require('path')
 const express = require('express')
-const app = express() // create express app
+const app = express()
 
-// add middlewares
 app.use(express.static(path.join(__dirname, 'build')))
-// app.use(express.static("public"));
-
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
 })
-
-// start express server on port 5000
 app.listen(5000, () => {
 	console.log('server started on port 5000')
 })
